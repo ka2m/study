9ef66a2ac3576d9d96ae3179bb344530c0247d0e
Vlad Selpukhin <slp.vld@gmail.com>
2014-09-24 19:14:08 +0400

    Update haskell task 9

--- Haskell/task9-02-1.hs
+++ Haskell/task9-02-1.hs
@@ -1,11 +1,11 @@
 cuttingSum :: Num a => [a] -> [a]
-cuttingSum l = 
-  let         
-    cuttingSum_iter [] res = reverse (res)
-    cuttingSum_iter ll res =
-      cuttingSum_iter (tail ll) ((foldl (+) 0 ll) : res)
+cuttingSum l =
+  let
+    cuttingSum_iter [] _ res = reverse (res)
+    cuttingSum_iter (x : xs) fs res =
+      cuttingSum_iter xs (fs - x) (fs : res)
   in
-    cuttingSum_iter l []
+    cuttingSum_iter l (foldl (+) 0 l) []
 
 
 main = do
--- Haskell/task9-02-2.hs
+++ Haskell/task9-02-2.hs
@@ -1,8 +1,11 @@
-cuttingSum :: Num a => [a] -> [a]
-cuttingSum []        = []
-cuttingSum [x]       = [x]
+cuttingSum :: Num a => [a] -> a -> [a]
+cuttingSum [] _       = []
+cuttingSum [x] _      = [x]
 -- here goes list concatenation instead of append an item
-cuttingSum l@(x:xs)  = [(foldl (+) 0 l)] ++ cuttingSum xs
+cuttingSum (x:xs) sum  = [sum] ++ (cuttingSum xs (sum-x))
+ 
 
 main = do
-  print $ cuttingSum [1,2,3]
+  -- used same thing twice as neither GHCi, nor Hugs
+  -- allow using variables in interpretation mode  
+  print $ cuttingSum [1,2,3] (foldl (+) 0 [1,2,3])
