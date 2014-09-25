00921d7fd72f7230025c6763febe46201dfa8734
Vlad Selpukhin <slp.vld@gmail.com>
2014-09-25 10:06:14 +0400

    Update Haskell for new reqs

--- Haskell/task9-02-2.hs
+++ Haskell/task9-02-2.hs
@@ -1,11 +1,13 @@
-cuttingSum :: Num a => [a] -> a -> [a]
-cuttingSum [] _       = []
-cuttingSum [x] _      = [x]
--- here goes list concatenation instead of append an item
-cuttingSum (x:xs) sum  = [sum] ++ (cuttingSum xs (sum-x))
+cuttingSumWrapper :: Num a => [a] -> [a]
+cuttingSumWrapper l =
+  let     
+    cuttingSum [] _       = []
+    cuttingSum [x] _      = [x]
+    -- here goes list concatenation instead of append an item
+    cuttingSum (x:xs) sum  = [sum] ++ (cuttingSum xs (sum-x))
+  in
+    cuttingSum l (foldl (+) 0 l)
  
 
 main = do
-  -- used same thing twice as neither GHCi, nor Hugs
-  -- allow using variables in interpretation mode  
-  print $ cuttingSum [1,2,3] (foldl (+) 0 [1,2,3])
+  print $ cuttingSumWrapper [1,2,3] 
