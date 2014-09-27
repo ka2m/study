f9d5fee9f1d6b6703f0a8aad0c1a5fa921d293d3
Vlad Selpukhin <slp.vld@gmail.com>
2014-09-27 14:55:53 +0400

    Complete dfs

---
+++ graph/dfs.py
@@ -0,0 +1,27 @@
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+
+def dfs_paths(graph, start, goal, path=None):
+    if path is None:
+        path = [start]
+    if start == goal:
+        yield path
+    for next in graph[start] - set(path):
+        yield from dfs_paths(graph, next, goal, path + [next])
+
+
+print(list(dfs_paths(graph, 'C', 'B')))
