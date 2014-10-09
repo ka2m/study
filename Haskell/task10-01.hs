d550597ed0609fab9fbba92f946d25172bcc1e99
Vlad Selpukhin <slp.vld@gmail.com>
2014-10-09 23:09:50 +0400

    Update Task 10

--- Haskell/task10-01.hs
+++ Haskell/task10-01.hs
@@ -8,24 +8,36 @@ derivation f =
 
 
 inverseFun :: (Ord a, Fractional a) => (a -> a) -> a -> a
-inverseFun f x = iter 0 1.0 0.0 (\x -> x)
+inverseFun f x = iter 0 0.0 (\x -> x) 0.0 1.0
     where
         x0 = 3.0
         eps = 0.001
-        evalA' prev k = 
-            if k == 0
-            then \x -> x
-            else \x -> ( derivation prev x / derivation f x )
+        elemB = x - f x0
+        
+        -- get new A_k
+        evalA prev k 
+            | k == 0    = \x -> x
+            | otherwise = \x -> ( derivation prev x / derivation f x )
+            
+        -- get new (x - F(x_0))^k
+        evalB b k 
+            | k == 0    = 1.0
+            | k == 1    = elemB
+            | otherwise = b * elemB
 
-        iter k prev sum' elemA =
-            let elemB = prev * (x - f x0) / 
-                        fromIntegral (if k == 0 then 1 :: Int else k)
-                newf = evalA' elemA k
-                newItem = newf x0 * elemB
+        iter k sum' prevA prevB prevBdenom =
+            let 
+                newB = evalB prevB k                
+                -- newB/k!
+                newBdenom = if k == 0 then 1.0 else prevBdenom*k
+                
+                newA = evalA prevA k                
+                
+                newItem = ( newA x0 ) * newB / newBdenom
             in  
                 if abs newItem < eps
                 then sum'
-                else iter (k + 1) elemB (sum' + newItem) newf
+                else iter (k + 1) (sum' + newItem) newA newB newBdenom 
 
 
 f1 x = 1.0 * sqrt(x)
