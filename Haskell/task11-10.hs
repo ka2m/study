96620d5af59dc9dc9d6f41c92fa4c6c933fce330
Vlad Selpukhin <slp.vld@gmail.com>
2014-10-15 01:42:07 +0400

    Task 11 update from Macbook

--- Haskell/task11-10.hs
+++ Haskell/task11-10.hs
@@ -1,13 +1,26 @@
-primedivisors :: [[Int]]
-primedivisors = map divs [1 ..]
-  where    
-    -- Generate infinite list of prime numbers
-    primes = sieve [2..]
-    sieve (p : xs) = p : sieve [x | x <- xs, x `mod` p > 0]
-    -- Append only matching divisors and 1. Map is better
-    -- as sieve in primes is using recursion
-    divs x =  1 : [y | y <- take x primes, x `rem` y == 0] 
+isPrime' :: Integer -> Integer -> Bool
+isPrime' x m 
+  | (m > (floor . sqrt . fromInteger) x) = True 
+  | otherwise = ((x `mod` m) /= 0) && (isPrime' x (m + 1))  
+ 
+isPrime :: Integer -> Bool
+isPrime x = isPrime' x 2
 
-main = do
-  print $ take 4 primedivisors
-  print $ take 6 primedivisors
+pl' :: Integer -> Integer -> [Integer] -> [Integer]
+pl' n m x 
+  | (head x) > n = tail x
+  | otherwise = if (isPrime m) 
+                then pl' n (m + 1) (m : x)
+                else pl' n (m + 1) x 
+ 
+pl :: Integer -> [Integer]
+pl n = pl' n 3 [2]
+                                 
+divsl :: Integer -> [Integer]
+divsl n = 1 : reverse ( filter (\x -> (n `mod` x) == 0) (pl n) )
+                                 
+primedivs' :: [Integer] -> [[Integer]]
+primedivs' = map (\ x -> divsl x) 
+
+primedivs :: [[Integer]]
+primedivs = primedivs' [2..]
