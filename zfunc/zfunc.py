e387b325e7caeb1218681d6eb852180ee60afa56
Vlad Selpukhin <slp.vld@gmail.com>
2014-11-11 00:09:00 +0300

    Modified zfunc to PEP

--- zfunc/zfunc.py
+++ zfunc/zfunc.py
@@ -1,22 +1,22 @@
 def ineffective_z_func(s):
-    res = [0]*len(s)
+    res = [0] * len(s)
     for i in xrange(1, len(s)):
-        while i + res[i] < len(s) and s[res[i]] == s[i+res[i]]:
+        while i + res[i] < len(s) and s[res[i]] == s[i + res[i]]:
             res[i] += 1
 
     return res
 
 def effective_z_func(s):
-    res = [0]*len(s)
+    res = [0] * len(s)
     l = 0
     r = 0
     for i in xrange(1, len(s)):
         if i <= r:
-            res[i] = min(r-i+1, res[i-l])
-        while  i+res[i] < len(s) and s[res[i]] == s[i+res[i]]:
+            res[i] = min(r - i + 1, res[i - l])
+        while  i + res[i] < len(s) and s[res[i]] == s[i + res[i]]:
             res[i] += 1
 
-        t = i+res[i]-1
+        t = i + res[i] - 1
         if (t > r):
             l = i
             r = t
