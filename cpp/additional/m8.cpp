6c55bdf9ccd97cdedd91432f3a6ef7afd6758d4c
Vlad Selpukhin <slp.vld@gmail.com>
2014-12-19 00:44:48 +0300

    no more recursion

--- cpp/additional/m8.cpp
+++ cpp/additional/m8.cpp
@@ -22,16 +22,26 @@ int** iter(int** a, int** b, int n)
 }
 
 
-void power(int** a, int** b, int n, int current, int s)
+int** power(int** a, int** b, int n, int s)
 {
-  if (current == s) {
-    return;
+
+  int** res = new int* [n];
+  for (int i = 0; i < n; i++) {
+    res[i]=new int[n];
+    for (int j=0;j<n;j++) 
+      res[i][j] = 0;
   }
-  else
-  {    
-    current++;
-    power(iter(a, b, n), b, n, current, s);
+
+  res = iter(a, b, n);
+
+  if (s == 1) return res;
+  
+  for (int i = 2; i < s; i++) {
+    res = iter(res, b, n);
   }
+
+  return res;
+
 }
 
 
@@ -46,9 +56,11 @@ int main (void)
   
   int** a = new int *[n];
   int** b = new int *[n];
+  int** r = new int *[n];
   for (int i = 0; i < n; i++) {
-    a[i]=new int[n];
-    b[i]=new int[n];    
+    a[i] = new int[n];
+    b[i] = new int[n];
+    r[i] = new int[n];
   }
   
   int s;
@@ -58,13 +70,14 @@ int main (void)
       {
         in >> a[i][j];
         b[i][j] = a[i][j];
+        r[i][j] = 0;
       }
   
-  power(a, b, n, 1, s);
+  r = power(a, b, n, s);
 
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++)
-      cout << a[i][j] << " ";
-    cout << endl;
+      out << r[i][j] << " ";
+    out << endl;
   }
 }
