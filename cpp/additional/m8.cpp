19834f23002920035e9c7a54439f747f0dbcf12a
Vlad Selpukhin <slp.vld@gmail.com>
2014-12-19 00:30:17 +0300

    WIP m8, not returning from rec

--- cpp/additional/m8.cpp
+++ cpp/additional/m8.cpp
@@ -2,25 +2,38 @@
 #include <fstream>
 using namespace std;
 
-int** power(int** a, int** b, int n, int s)
+
+int** iter(int** a, int** b, int n)
 {
-  int **res = new int *[n];
+  int** res = new int* [n];
 
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++) {
     res[i]=new int[n];
-
-  for (int i=0;i<n;i++)
     for (int j=0;j<n;j++)
       res[i][j] = 0;
+  }
 
-  for ( int m = 1; m < s; m++)
-    for (int row = 0; row < n; row++)
-      for (int col = 0; col < n; col++)
-        for (int inner = 0; inner < n; inner++)
-          res[row][col] += a[row][inner] * b[inner][col];
+  for (int row = 0; row < n; row++)
+    for (int col = 0; col < n; col++)
+      for (int inner = 0; inner < n; inner++)
+        res[row][col] += a[row][inner] * b[inner][col];
 
   return res;
 }
+
+
+void power(int** a, int** b, int n, int current, int s)
+{
+  if (current == s) {
+    return;
+  }
+  else
+  {    
+    current++;
+    power(iter(a, b, n), b, n, current, s);
+  }
+}
+
 
 int main (void)
 {
@@ -31,12 +44,11 @@ int main (void)
   int n;
   in >> n;
   
-  int **a= new int *[n];
-  int **b= new int *[n];
-
+  int** a = new int *[n];
+  int** b = new int *[n];
   for (int i = 0; i < n; i++) {
     a[i]=new int[n];
-    b[i]=new int[n];
+    b[i]=new int[n];    
   }
   
   int s;
@@ -47,12 +59,12 @@ int main (void)
         in >> a[i][j];
         b[i][j] = a[i][j];
       }
-
-  int **r = power(a,b, n, s);
   
-  for ( int i = 0; i < n; i++) {
-    for ( int j = 0; j < n; j++)
-      out << r[i][j] << " ";
-    out << endl;
+  power(a, b, n, 1, s);
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++)
+      cout << a[i][j] << " ";
+    cout << endl;
   }
 }
