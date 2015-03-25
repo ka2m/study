08819d04066410e7c76bea0c77b00fcf1872703f
Vlad Selpukhin <slp.vld@gmail.com>
2015-03-25 22:56:13 +0300

    [GRAPH] Fix typo in 2_9

--- graph-4sem/task2_9.py
+++ graph-4sem/task2_9.py
@@ -1,24 +1,18 @@
 from graphfau import BaseGraph
 import copy
 
- def removeEvenVertexes(graph):
-     g2 = copy.deepcopy(graph)
-     flag = False
-     for v in graph.vertexes:
-         if not graph.degree(v) % 2:
-             g2.remove_vertex(v)
-             flag = True
-    if not flag:
-         print('No even vertexes found')
-    return g2
-
 
 def removeEvenVertexes(graph):
     g2 = copy.deepcopy(graph)
+    flag = False
     for v in graph.vertexes:
         if not graph.degree(v) % 2:
             g2.remove_vertex(v)
+            flag = True
+    if not flag:
+        print('No even vertexes found')
     return g2
+
 
 g = BaseGraph.BaseGraph('data')
 print(g)
