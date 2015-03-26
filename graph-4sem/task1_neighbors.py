710b28695620d64469b34e7defe1006cde9b887e
Vlad Selpukhin <slp.vld@gmail.com>
2015-03-26 23:53:38 +0300

    [GRAPH] Fix and rename 1_15 for new graph

--- graph-4sem/task1_neighbors.py
+++ graph-4sem/task1_neighbors.py
@@ -1,15 +1,15 @@
-from graphfau import BaseGraph
+from graphfau.BaseGraph import BaseGraph
 
 
 def nbs(graph, vertex1, vertex2):
     list1 = graph.get_adjlist_by_vertex(vertex1)
     list2 = graph.get_adjlist_by_vertex(vertex2)
-    res = list(set(list1).intersection(set(list2)))
+    res = list(list1.intersection(list2))
     if not res:
         print('No neighbours!')
     return res
 
-g = BaseGraph.BaseGraph('data')
+g = BaseGraph(filename='data')
 print(g)
 print('----')
 
