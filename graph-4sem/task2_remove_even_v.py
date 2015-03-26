8fc6881db93b7c8a1355365813fc63c13cb395a2
Vlad Selpukhin <slp.vld@gmail.com>
2015-03-26 23:58:19 +0300

    [GRAPH] Fix and rename 2_9

--- graph-4sem/task2_remove_even_v.py
+++ graph-4sem/task2_remove_even_v.py
@@ -2,19 +2,19 @@ from graphfau import BaseGraph
 import copy
 
 
-def removeEvenVertexes(graph):
+def remove_even_vertices(graph):
     g2 = copy.deepcopy(graph)
     flag = False
-    for v in graph.vertexes:
+    for v in graph.vertices:
         if not graph.degree(v) % 2:
             g2.remove_vertex(v)
             flag = True
     if not flag:
-        print('No even vertexes found')
+        print('No even vertices found')
     return g2
 
 
-g = BaseGraph.BaseGraph('data')
+g = BaseGraph.BaseGraph(filename='data')
 print(g)
-g2 = removeEvenVertexes(g)
+g2 = remove_even_vertices(g)
 print(g2)
