fce31d5b1a8ee7b74474f888107684e901ad0a44
Vlad Slepukhin <slp.vld@gmail.com>
2015-11-06 00:21:07 +0300

    [NM] Update Gauss

--- numerical-methods/task5_gauss.py
+++ numerical-methods/task5_gauss.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 
 
+def swap_rows(llist, i, row, count):
+    tmp = llist[i]
+    llist[i] = llist[row]
+    llist[row] = tmp
+
+
 def asum(xl, al, k, size):
     res = 0.0
     for j in range(k, size):
@@ -8,7 +14,7 @@ def asum(xl, al, k, size):
     return res
 
 
-def directFlow(al, bl, size):
+def direct_flow(al, bl, size):
     t = []
     for i in range(0, size):
         l = []
@@ -24,7 +30,7 @@ def directFlow(al, bl, size):
                 al[i][j] -= t[i][k] * al[k][j]
 
 
-def reverseFlow(xl, al, bl, size):
+def reverse_flow(xl, al, bl, size):
     xl[size - 1] = bl[size - 1] / al[size - 1][size - 1]
     for k in reversed(range(0, size - 1)):
         xl[k] = (bl[k] - asum(xl, al, k, size)) / al[k][k]
@@ -33,14 +39,34 @@ def reverseFlow(xl, al, bl, size):
 def solve(al, bl):
     size = len(al)
     xl = [0.0 for x in range(size)]
-    directFlow(al, bl, size)
-    reverseFlow(xl, al, bl, size)
+    i = 0
+    count = 0
+    for i in range(0, size):
+        max_el = -1.0
+        row = -1
+        for j in range(i, size):
+            if abs(al[j][i]) > max_el:
+                max_el = abs(a[j][i])
+                row = j
+        if not int(max_el):
+            raise ValueError('Column has max element == zero')
+
+        count = 0
+        if i != row:
+            count += 1
+            swap_rows(al, i, row, count)
+            swap_rows(bl, i, row, count)
+        print 'swaps: ', count
+    direct_flow(al, bl, size)
+    reverse_flow(xl, al, bl, size)
     return xl
 
 if __name__ == "__main__":
-    a = [[2.0, -5.0, 3.0],
+    a = [[0.0, -5.0, 3.0],
          [4.0, 0.0, -1.0],
-         [-9.0, 6.0, 2.0]]
+         [-9.0, 6.0, 0.0]]
     b = [3.0, -1.0, 2.0]
     print 'solve'
+    print a
+    print b
     print solve(a, b)
