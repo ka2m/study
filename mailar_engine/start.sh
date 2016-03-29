4de36e8ce7efe3f968b0fc1ec5653d0d93d6c417
Vlad Slepukhin <slp.vld@gmail.com>
2016-03-30 00:31:06 +0300

    [ENGINE] Improve Dockerfile

--- mailar_engine/start.sh
+++
@@ -1,8 +0,0 @@
-#TBD
-sleep 6
-python manage.py makemigrations registration_api
-python manage.py makemigrations mailar_engine
-python manage.py migrate
-#echo "from django.contrib.auth.models import User; User.objects.create_superuser('vladfau', 'admin@mailar.com', 'pass')" | ./manage.py shell
-
-python manage.py test --keepdb
