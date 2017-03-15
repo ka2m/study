75247f768c2f6995c6fe7c2b1f452bf2c52aa095
Vlad Slepukhin <slp.vld@gmail.com>
2017-03-15 14:38:49 +0400

    [MODEL] Fix 222

--- modeling/task222.py
+++ modeling/task222.py
@@ -15,22 +15,23 @@ real_sigma = 0.03
 
 customer_sigma = 0.1
 
-all_customer_choices = []
 count_wins = 0
+all_real_bags = []
 
 for _ in range(max_tries):
     real_bags = [rand_normal(real_mu, real_sigma)
                  for _ in range(max_bags)]
+    all_real_bags.append(max(real_bags))
 
-    customer_choice = max([rand_normal(rbw, customer_sigma)
-                           for rbw in real_bags])
-    all_customer_choices.append(customer_choice)
-    if customer_choice >= max(real_bags):
+    customer_choice = [rand_normal(rbw, customer_sigma)
+                       for rbw in real_bags]
+
+    if real_bags.index(max(real_bags)) == customer_choice.index(max(customer_choice)):
         count_wins += 1
 
-answer = round(reduce(lambda x, y: x + y, all_customer_choices) / max_tries, 3)
+answer = round(reduce(lambda x, y: x + y, all_real_bags) / max_tries, 3)
 
 print("Оценка мат. ожидания веса <<тяжелого>> мешка: ", answer)
 
 print("Вероятность выбора самого тяжелого мешка: ",
-      round(count_wins / max_tries, 2))
+      round(count_wins / max_tries, 3))
